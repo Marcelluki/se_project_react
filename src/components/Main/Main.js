f2130df9cf0dfc707236b2bb252608b3fb941953
Marcelluki <marcell.rangel29@gmail.com>
2024-02-21 20:26:56 -0800

    added F and C variable with calculations

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -14,25 +14,46 @@ function Main({
   const { currentTemperatureUnit } = useContext(CurrentTemperatureUnitContext);
   console.log(currentTemperatureUnit);
   const temp = weatherTemp?.temperature?.[currentTemperatureUnit] || 999;
+
   const weatherType = useMemo(() => {
-    if (currentTemperatureUnit === "F") {
-      if (temp >= 86) {
+    const tempInF = currentTemperatureUnit === "F" ? temp : temp * 1.8 + 32;
+    const tempInC = currentTemperatureUnit === "C" ? temp : (temp - 32) / 1.8;
+    if (tempInF >= 86) {
+      return "hot";
+    } else if (tempInF >= 66 && tempInF <= 85) {
+      return "warm";
+    } else if (tempInF <= 65) {
+      return "cold";
+    } else {
+      if (tempInC >= 30) {
         return "hot";
-      } else if (temp >= 66 && temp <= 85) {
+      } else if (tempInC >= 30 && tempInC <= 29) {
         return "warm";
-      } else if (temp <= 65) {
+      } else if (tempInC <= 18) {
         return "cold";
-      } else {
-        if (temp >= 30) {
-          return "hot";
-        } else if (temp >= 30 && temp <= 29) {
-          return "warm";
-        } else if (temp <= 18) {
-          return "cold";
-        }
       }
     }
   }, [weatherTemp]);
+
+  // const weatherType = useMemo(() => {
+  //   if (currentTemperatureUnit === "F") {
+  //     if (temp >= 86) {
+  //       return "hot";
+  //     } else if (temp >= 66 && temp <= 85) {
+  //       return "warm";
+  //     } else if (temp <= 65) {
+  //       return "cold";
+  //     } else {
+  //       if (temp >= 30) {
+  //         return "hot";
+  //       } else if (temp >= 30 && temp <= 29) {
+  //         return "warm";
+  //       } else if (temp <= 18) {
+  //         return "cold";
+  //       }
+  //     }
+  //   }
+  // }, [weatherTemp]);
 
   const filteredCards = items.filter((item) => {
     return item.weather.toLowerCase() === weatherType;
